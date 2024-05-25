6d13f89bca259c4bfc75709c7dde17d6a0082565
Alan Steremberg <alan.steremberg@gmail.com>
2024-05-25 15:57:27 -0400

    new sys, release

--- Arcade-SuperBreakout.sv
+++ Arcade-SuperBreakout.sv
@@ -183,6 +183,17 @@ assign LED_USER  = ioctl_download;
 assign FB_FORCE_BLANK = '0;
 assign HDMI_FREEZE = 0;
 
+assign ADC_BUS  = 'Z;
+assign USER_OUT = '1;
+assign {UART_RTS, UART_TXD, UART_DTR} = 0;
+assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
+assign {SDRAM_DQ, SDRAM_A, SDRAM_BA, SDRAM_CLK, SDRAM_CKE, SDRAM_DQML, SDRAM_DQMH, SDRAM_nWE, SDRAM_nCAS, SDRAM_nRAS, SDRAM_nCS} = 'Z;
+
+assign AUDIO_MIX = 0;
+
+assign BUTTONS = 0;
+
+
 wire [1:0] ar = status[15:14];
 
 
